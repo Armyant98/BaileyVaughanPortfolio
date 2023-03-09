4caebfd1f5732a5f22b84271212b336d96691948
Armyant98 <102646883+Armyant98@users.noreply.github.com>
2023-03-08 20:01:44 -0500

    scroll

---
+++ assets/js/scrollAnimations.js
@@ -0,0 +1,75 @@
+
+let scrollpos = window.scrollY;
+const aboutSection = document.getElementById("slideRight");
+const programIcons = document.querySelectorAll(".programs");
+
+function add_class_on_scroll() {
+    aboutSection.classList.add("slideRight");
+    
+}
+
+function remove_class_on_scroll() {
+    aboutSection.classList.remove("slideRight");
+   
+}
+
+function icon_on_scroll(){
+  programIcons.forEach((element) => {
+    element.classList.add('flip');
+  });
+}
+
+function remove_icon_on_scroll(){
+  programIcons.forEach((element) => {
+    element.classList.remove('flip');
+  });
+}
+
+// slide righ animation
+window.addEventListener('scroll', function(){ 
+    //Here you forgot to update the value
+    scrollpos = window.scrollY;
+
+    if(scrollpos > 200){
+        add_class_on_scroll();
+    }
+    
+    else {
+        remove_class_on_scroll();
+    }
+    console.log(scrollpos);
+});
+
+// icons animation 
+
+window.addEventListener('scroll', function(){ 
+  //Here you forgot to update the value
+  scrollpos = window.scrollY;
+
+  if(scrollpos > 1050){
+      icon_on_scroll();
+  }
+  
+  else {
+      remove_icon_on_scroll();
+  }
+  console.log(scrollpos);
+});
+
+const container = document.querySelector('.container')
+const sections = gsap.utils.toArray(".container .panel");
+
+
+let horizontalScroll = gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    end: "+=2000"
+  }
+});
+
+
+
